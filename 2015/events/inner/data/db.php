4ac3974a8350bc89436f04e782baa242e8beaff2
Minhaz A V <minhazav@gmail.com>
2015-02-14 23:25:43 +0530

    useless files removed

--- 2015/events/inner/data/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$server='cistonerdejavu.db.12108205.hostedresource.com';
-	$username='cistonerdejavu';
-	$password='muStafa786#';
-	$db_name='cistonerdejavu';
-	$db_table='website_content';
-	$db_table2='event_list';
-?>
