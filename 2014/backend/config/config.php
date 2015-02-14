398cf4405c45a193e711f40da13cb7a622afaa16
Minhaz A V <minhazav@gmail.com>
2015-02-14 23:27:29 +0530

    random info changes

--- 2014/backend/config/config.php
+++ 2014/backend/config/config.php
@@ -11,9 +11,8 @@
  }
 define("HOST","localhost");
 define("USER","engifest");
-define("PASSWORD","WGQ2DTzM9RP2vnGq");
-define("_DB_MAIN","engifest_2014");
+define("PASSWORD","---");
+define("_DB_MAIN","---");
 define("username_key",md5("mmt_username"));
 define("password_key",md5("mmt_password"));
 define("password_salt","temporarypasswordsalt");
-define("domain","cistoner.org");
